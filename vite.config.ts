ff4e2ee8429667da14ea9e23dc7364823e974a41
alexlibe95 <alexlibe95@gmail.com>
2025-09-21 00:56:11 +0300

    finilize pwa2

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,18 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
+            src: '/icon-192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          },
+          {
             src: '/favicon.ico',
             sizes: '64x64 32x32 24x24 16x16',
             type: 'image/x-icon'
